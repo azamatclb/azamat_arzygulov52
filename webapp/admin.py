83e7d9aa489ae3c243335922dc875f75f1f95373
azamat <azamatclb@gmail.com>
2024-06-30 13:42:02 +0600

    Создал модель TaskManage и миграцию

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from webapp.models import TaskManage
+
+
+class TaskManageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'deadline']
+
+
 # Register your models here.
+
+admin.site.register(TaskManage, TaskManageAdmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
+class TaskManage(models.Model):
+    description = models.CharField(max_length=200, blank=False, null=False, verbose_name="Описание")
+    status = models.CharField(max_length=200, choices=status_choices, verbose_name="Статус")
+    deadline = models.DateField(verbose_name='Дедлайн')
+
+    def __str__(self):
+        return f"{self.id}. {self.description} {self.status} {self.deadline}"
+
+    class Meta:
+        db_table = "tasks list"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
